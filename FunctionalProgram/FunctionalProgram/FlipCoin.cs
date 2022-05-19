919a54a9e3bb027cc8d5e9b3680c745abebbee36
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-19 20:05:43 +0530

    [Madhavi] added program for flip coin

---
+++ FunctionalProgram/FunctionalProgram/FlipCoin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    internal class FlipCoin
+    {
+        int heads = 0, tails = 0;
+        double headspercent = 0.0, tailspercent = 0.0;
+        Random Rand = new Random();
+        public void HeadTail()
+        {
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int coin = Rand.Next(0, 2);
+
+                if (coin == 1)
+                {
+                    heads += 1;
+                }
+                else
+                {
+                    tails += 1;
+                }
+            }
+            headspercent = Convert.ToDouble(heads / 10.0);
+            tailspercent = Convert.ToDouble(100.0 - headspercent);
+
+            Console.WriteLine($"Heads = {headspercent} ");
+            Console.WriteLine($"Tails = {tailspercent} ");
+        }
+    }
+}
---
+++ FunctionalProgram/FunctionalProgram/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FunctionalProgram;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of Two\n4.Harmonic Number\n5.Prime Factors" +
+            "\n6.Quotient and Remainder\n7.Swap Numbers\n8.Even Odd\n9.Vowels and Consonant\nLargest Number");
+        Console.WriteLine("Enter option ");
+        int option = Convert.ToInt32(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                FlipCoin flip_coin = new FlipCoin();
+                flip_coin.HeadTail();
+                break;
+            case 2:
+                //basic.HeadTail();
+                break;
+            case 3:
+                //basic.HeadTail();
+                break;
+            case 4:
+                //basic.HeadTail();
+                break;
+            case 5:
+                //basic.HeadTail();
+                break;
+            case 6:
+                //basic.HeadTail();
+                break;
+            case 7:
+                //basic.HeadTail();
+                break;
+            case 8:
+                //basic.HeadTail();
+                break;
+            case 9:
+                //basic.HeadTail();
+                break;
+            case 10:
+                //basic.HeadTail();
+                break;
+            default:
+                //basic.HeadTail();
+                break;
+
+        }
+    }
+}
